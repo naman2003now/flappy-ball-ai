2c777e172d3f367755d3aae25e4e7bd94ccafe21
Naman Agrawal <naman2003now@gmail.com>
2024-06-21 14:20:11 +0530

    display the ball with it's sprite on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+
+const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.8);
+const BALL_COLOR: Color = Color::rgb(0.8, 0.1, 0.2);
+
+const BALL_RADIUS: f32 = 25.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .run();
+}
+
+#[derive(Component)]
+struct Ball;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Circle {
+                radius: BALL_RADIUS,
+            })),
+            material: materials.add(BALL_COLOR),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+        Ball,
+    ));
 }
